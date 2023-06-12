2cd3e6543f47ccf8899914d516f271d88c04cccd
Uzzal Kar <uzzalkar12@gmail.com>
2023-06-12 23:28:16 +0600

    ...

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const url = 'https://memories-app-server-phi.vercel.app/posts';
+
+// const url = 'https://memories-app-server-phi.vercel.app/posts';
+const url = 'http://localhost:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
